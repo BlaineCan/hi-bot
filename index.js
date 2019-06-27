553e9d8221626f6a60178ae9074351dfb2dc7663
Whatchamazog <blaine.cantrell@gmail.com>
2019-06-26 21:33:16 -0500

    small improvements

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const config = require("./config");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let greetings = ["Hi", "Hi!", "Hi :slight_smile:", "Hi! :slight_smile:"]
-let greet = greetings[Math.floor(Math.random() * greetings.length)];
-
-//setInterval(sayHi(), 60000);
-
 client.on("ready", () =>{
   client.user.setPresence({
     game: {
@@ -20,9 +15,11 @@ client.on("ready", () =>{
 client.on("message", message => {
   if(message.author.bot) return;
   if(message.channel.id !== config.chan) return;
-  if(message.content.includes("hi") || message.content.includes("Hi")){
+  if(message.content.includes(" hi ") || message.content.includes(" Hi ")){
     return;
   } else {
+    console.log(`${message.author.tag} said ${message.content}. This is not allowed.`)
+    message.delete();
     message.reply("this channel is used for saying Hi. :slight_smile:");
   }
 })
