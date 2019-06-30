f371f72aca23e781d1f839e50b30a5cc7d138929
Whatchamazog <blaine.cantrell@gmail.com>
2019-06-30 00:45:37 -0500

    Made things good

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ client.on("ready", () =>{
     },
     status: 'online'
   })
+  setInterval(() => {
+    client.channels.get(config.chan.hi).send("Hi!")
+  }, 2500)
 })
 
 client.on("message", message => {
   if(message.author.bot) return;
   if(message.channel.id !== config.chan.hi) return;
-  if(message.content.includes("hi ") || message.content.includes("Hi ") || message.content.includes("hi!") || message.content.includes("Hi!") || message.content.includes("hi.") || message.content.includes("Hi.") || message.content.length < 3){
+  if(message.content === "Hi!" ) {
     return;
   } else {
     console.log(`${message.author.tag} said ${message.content}. This is not allowed.`)
     message.delete();
-    message.reply("this channel is used for saying Hi. :slight_smile:");
+    message.reply("this channel is used for saying Hi! :slight_smile:");
   }
 })
 
