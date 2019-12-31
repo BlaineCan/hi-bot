264a58e9d7002e82acc4ebd30f378479a8747705
Whatchamazog <blaine.cantrell@gmail.com>
2019-12-30 18:12:14 -0600

    I did some stuff

--- hi-bot-main.go
+++ hi-bot-main.go
@@ -8,6 +8,9 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"os"
+	"syscall"
+	"os/signal"
 )
 
 var (
@@ -16,24 +19,27 @@ var (
 )
 
 func main(){
+	/* This is just a quick sanity check to make sure that a token is present. */
+	if token == "" {
+		fmt.Println("No token provided. Please provide a valid token to continue.")
+		return
+	}
 
 	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println(err.Error())
 		return 
 	}
-	
 	err = discord.Open()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	
+	/* Hang out here until CTRL-C */
 	fmt.Println("Bonfire lit. Press CRTL-C to exit.")
-	
-	<-make(chan struct{})
-	return 
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
 }
-
-
-
 
